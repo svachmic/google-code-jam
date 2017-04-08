517149f30e108e1f6032d0bbf05f343fadcc6b29
Michal Švácha <michal.svacha@gmail.com>
2017-04-08 20:23:22 +0200

    Last tidy number search

--- tidy-numbers/src/main/java/Number.java
+++ tidy-numbers/src/main/java/Number.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by michalsvacha on 08.04.17.
  */
@@ -13,11 +15,11 @@ public class Number {
     public String toString() {
         return "Number{" +
                 "number=" + number +
-                ",tidy=" + isNumberTidy() +
+                ",tidy=" + isNumberTidy(this.number) +
                 '}';
     }
 
-    private boolean isNumberTidy() {
+    private boolean isNumberTidy(long number) {
         char[] numberArray = ("" + number).toCharArray();
         char tmp = numberArray[0];
         for (int i = 1; i < numberArray.length; i++) {
@@ -31,5 +33,46 @@ public class Number {
         }
 
         return true;
+    }
+
+    public long getLastTidyNumber() {
+        return this.tidyNumberSearch(this.number);
+    }
+
+    private long tidyNumberSearch(long number) {
+        char[] numberArray = ("" + number).toCharArray();
+        for (int i = 0; i < numberArray.length; i++) {
+            if (numberArray[i] == '0') {
+                if (i + 1 == numberArray.length) {
+                    return tidyNumberSearch(number - 1);
+                } else {
+                    char[] subtractArray = Arrays.copyOfRange(numberArray, i, numberArray.length);
+                    long subtract = Long.parseLong(String.valueOf(subtractArray)) + 1;
+                    return tidyNumberSearch(number - subtract);
+                }
+            }
+        }
+
+        char tmp = numberArray[numberArray.length - 1];
+        for (int i = numberArray.length - 2; i > -1; i--) {
+            char c = numberArray[i];
+            if (c > tmp) {
+                long multiplicator = (long) Math.pow(10, i - 1);
+                long subtract = ((long) (Character.getNumericValue(tmp) + 1) * multiplicator);
+                long newNumber = number - subtract;
+
+                char[] newNumberArray = ("" + newNumber).toCharArray();
+                for (int j = i + 1; j < newNumberArray.length; j++) {
+                    newNumberArray[j] = '9';
+                }
+                long normalizedNumber = Long.parseLong(String.valueOf(newNumberArray));
+
+                return tidyNumberSearch(normalizedNumber);
+            } else {
+                tmp = c;
+            }
+        }
+
+        return number;
     }
 }

a79bf726e3d63a2812779e749545fc0a6eb7ce72
Michal Švácha <michal.svacha@gmail.com>
2017-04-08 19:18:11 +0200

    Data parsing

--- tidy-numbers/src/main/java/Main.java
+++ tidy-numbers/src/main/java/Main.java
@@ -1,10 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by michalsvacha on 08.04.17.
  */
 
 public class Main {
+    static List<Number> testCases = new ArrayList<>();
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        String path = "src/main/resources/" + "sample.in";
+        readInput(path);
+        System.out.println(testCases);
+    }
+
+    private static void readInput(String file) {
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(file));
+
+            int testCasesCount = Integer.parseInt(bf.readLine());
+            for (int i = 0; i < testCasesCount; i++) {
+                String line = bf.readLine();
+
+                Number number = new Number(Long.parseLong(line));
+                testCases.add(number);
+            }
+        } catch (Exception e) {
+            System.out.println("Cannot read file");
+            e.printStackTrace();
+        }
     }
 }

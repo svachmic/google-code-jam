52147e4a54a6f779645f6fd52866983dddb5f220
Michal Švácha <michal.svacha@gmail.com>
2017-04-23 18:54:57 +0200

    #2 Implemented usage of ColorRules object
    Overdue

--- 2017/01_round_1b/stable-neigh-bors/src/main/java/TestCase.java
+++ 2017/01_round_1b/stable-neigh-bors/src/main/java/TestCase.java
@@ -5,6 +5,7 @@
 public class TestCase {
     private int total;
     private int red, orange, yellow, green, blue, violet;
+    private ColorRules rules = new ColorRules();
 
     public TestCase(int total, int red, int orange, int yellow, int green, int blue, int violet) {
         this.total = total;
@@ -70,16 +71,16 @@ public class TestCase {
 
     public String getRingPlacement() {
         String subSolution = this.getNeighbor(-1, this.getArrayRepresentation(), "", "IMPOSSIBLE");
-        return this.getPrettyOutput(subSolution);
+        return this.rules.getPrettyOutput(subSolution);
     }
 
     private String getNeighbor(int lastIndex, int[] counts, String placement, String bestPlacement) {
         if (placement.length() == this.getTotal()) {
-            return this.isValidSolution(placement) ? placement : bestPlacement;
+            return this.rules.isValidSolution(placement) ? placement : bestPlacement;
         }
 
         for (int i = 0; i < 6; i++) {
-            if (counts[i] > 0 && isGoodNeighbor(lastIndex, i)) {
+            if (counts[i] > 0 && this.rules.isGoodNeighbor(lastIndex, i)) {
                 counts[i] = counts[i] - 1;
                 String candidateSolution = this.getNeighbor(i, counts, placement + i, bestPlacement);
                 if (candidateSolution != "IMPOSSIBLE") {
@@ -90,52 +91,5 @@ public class TestCase {
         }
 
         return bestPlacement;
-    }
-
-    private boolean isGoodNeighbor(int placed, int candidate) {
-        if (placed == -1) {
-            return true;
-        } else if (placed == candidate) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    private String getPrettyOutput(String solution) {
-        if (solution == "IMPOSSIBLE") {
-            return solution;
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < solution.length(); i++) {
-            int index = Character.getNumericValue(solution.charAt(i));
-            sb.append(this.getStringRepresentation(index));
-        }
-        return sb.toString();
-    }
-
-    private String getStringRepresentation(int index) {
-        switch (index) {
-            case 0:
-                return "R";
-            case 1:
-                return "O";
-            case 2:
-                return "Y";
-            case 3:
-                return "G";
-            case 4:
-                return "B";
-            case 5:
-                return "V";
-            default:
-                return "";
-        }
-    }
-
-    private boolean isValidSolution(String solution) {
-        int first = Character.getNumericValue(solution.charAt(0));
-        int last = Character.getNumericValue(solution.charAt(this.getTotal() - 1));
-        return this.isGoodNeighbor(first, last);
     }
 }

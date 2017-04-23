f3031f24c43a0e4100224e5b23d613e45303e26c
Michal Švácha <michal.svacha@gmail.com>
2017-04-23 15:39:17 +0200

    #3 Semi-working solution
    Overdue

--- 2017/01_round_1b/pony-express/src/main/java/TestCase.java
+++ 2017/01_round_1b/pony-express/src/main/java/TestCase.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -48,7 +49,92 @@ public class TestCase {
     }
 
     private double shortestTime(int from, int to) {
-        return 0.0;
+        boolean[] visited = new boolean[this.cities.size()];
+        visited[from] = true;
+        return visit(from, to, visited, 0, 0, 0, 100);
+    }
+
+    private double visit(int city, int destination, boolean[] visited, double time, int remainingHorsePower, int speed, double best) {
+        City startCity = this.cities.get(city);
+
+        if (city == destination) {
+            return time < best ? time : best;
+        }
+
+        for (int i = 0; i < this.distances.length; i++) {
+            if (i != city && this.distances[city][i] != -1 && !visited[i]) {
+                double distanceToCity = (double) this.distances[city][i];
+
+                double thisCityHorseTime = distanceToCity / (double) startCity.getAverageHorseSpeed();
+                int hypothetical = startCity.getMaxHorseDistance() - (int) distanceToCity;
+
+                double oldHorseTime = distanceToCity / (double) speed;
+
+                if (remainingHorsePower < distanceToCity) {
+                    time += thisCityHorseTime;
+                    remainingHorsePower = hypothetical;
+                    speed = startCity.getAverageHorseSpeed();
+
+                    visited[i] = true;
+                    double a = visit(i, destination, visited, time, remainingHorsePower, speed, best);
+                    if (a < best) {
+                        best = a;
+                    }
+                    visited[i] = false;
+
+                } else {
+
+                    int tmpRemaining = remainingHorsePower;
+                    int tmpSpeed = speed;
+
+                    time += thisCityHorseTime;
+                    remainingHorsePower = hypothetical;
+                    speed = startCity.getAverageHorseSpeed();
+
+                    visited[i] = true;
+                    double a = visit(i, destination, visited, time, remainingHorsePower, speed, best);
+                    if (a < best) {
+                        best = a;
+                    }
+                    visited[i] = false;
+
+                    time -= thisCityHorseTime;
+                    time += oldHorseTime;
+                    remainingHorsePower = tmpRemaining;
+                    remainingHorsePower -= distanceToCity;
+                    speed = tmpSpeed;
+                    visited[i] = true;
+                    a = visit(i, destination, visited, time, remainingHorsePower, speed, best);
+                    if (a < best) {
+                        best = a;
+                    }
+                    visited[i] = false;
+
+
+                    //if (oldHorseTime < thisCityHorseTime) {
+                    //    time += oldHorseTime;
+                    //    remainingHorsePower -= distanceToCity;
+                    //} else {
+                    //    time += thisCityHorseTime;
+                    //    remainingHorsePower = hypothetical;
+                    //    speed = startCity.getAverageHorseSpeed();
+                    //}
+                }
+
+                //if (i == destination && time < best) {
+                //    best = time;
+                //} else {
+                //    visited[i] = true;
+                //    double a = visit(i, destination, visited, time, remainingHorsePower, speed, best);
+                //    if (a < best) {
+                //        best = a;
+                //    }
+                //    visited[i] = false;
+                //}
+            }
+        }
+
+        return best;
     }
 
     public String deliveryTimes() {

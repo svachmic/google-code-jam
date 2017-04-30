2c9e2e7cf5876d22d9eb00a8f29dddea1753faec
Michal Švácha <michal.svacha@gmail.com>
2017-04-30 12:50:58 +0200

    #4 Implemented brute force solution

--- 2017/02_round_1c/a-ample-syrup/src/main/java/TestCase.java
+++ 2017/02_round_1c/a-ample-syrup/src/main/java/TestCase.java
@@ -6,6 +6,7 @@ import java.util.List;
 /**
  * Created by michalsvacha on 30.04.17.
  */
+
 public class TestCase {
     int pancakeCount, orderedCount;
     List<Pancake> pancakes = new ArrayList<>();
@@ -13,6 +14,14 @@ public class TestCase {
     public TestCase(int pancakeCount, int orderedCount) {
         this.pancakeCount = pancakeCount;
         this.orderedCount = orderedCount;
+    }
+
+    public int getPancakeCount() {
+        return pancakeCount;
+    }
+
+    public int getOrderedCount() {
+        return orderedCount;
     }
 
     public void addPancake(Pancake pancake) {
@@ -34,6 +43,57 @@ public class TestCase {
     }
 
     public double getMaximumArea() {
-        return 0.0;
+        if (this.getOrderedCount() == this.getPancakeCount()) {
+            List<Integer> selectedPancakes = new ArrayList<>();
+            for (int i = 0; i < this.getOrderedCount(); i++) {
+                selectedPancakes.add(i);
+            }
+            return this.getArea(selectedPancakes);
+        }
+
+        return findOptimumCombination(new ArrayList<>(), 0, 0.0);
+    }
+
+    public double findOptimumCombination(List<Integer> selectedPancakes, int startIndex, double bestSolution) {
+        if (selectedPancakes.size() == this.getOrderedCount()) {
+            double area = this.getArea(selectedPancakes);
+            return area > bestSolution ? area : bestSolution;
+        }
+
+        if ((selectedPancakes.size() + (pancakes.size() - startIndex)) < this.getOrderedCount()) {
+            return bestSolution;
+        }
+
+        for(int i = startIndex; i < pancakes.size(); i++) {
+            selectedPancakes.add(i);
+            double candidateSolution = findOptimumCombination(selectedPancakes, startIndex + 1, bestSolution);
+            if (candidateSolution > bestSolution) {
+                bestSolution = candidateSolution;
+            }
+            selectedPancakes.remove(selectedPancakes.size() - 1);
+        }
+
+        return bestSolution;
+    }
+
+    private double getArea(List<Integer> selectedPancakes) {
+        double totalArea = 0.0;
+
+        Pancake first = this.pancakes.get(selectedPancakes.get(0));
+        totalArea += first.getSideArea();
+
+        for (int i = 1; i < selectedPancakes.size(); i++) {
+            Pancake next = this.pancakes.get(selectedPancakes.get(i));
+            totalArea += next.getSideArea();
+            totalArea += this.getPancakeDifference(first, next);
+            first = next;
+        }
+
+        totalArea += first.getTopArea();
+        return totalArea;
+    }
+
+    private double getPancakeDifference(Pancake bottom, Pancake top) {
+        return bottom.getTopArea() - top.getTopArea();
     }
 }
